c53e456cec2a29bc433db0bcf286459ba81ef735
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-09-08 17:59:19 +0200

    Redid Guest class + read users from (empty at the moment) file into an array

--- src/control/App.java
+++ src/control/App.java
@@ -5,11 +5,15 @@ import model.people.Guest;
 import model.utility.*;
 import view.MainWindow;
 
+import java.io.*;
 import java.util.Arrays;
+
 
 public class App {
 
     private double money = 0;
+    private Guest[] users;
+    private Storage storage;
 
     public static void main(String[] args) {
         new App();
@@ -18,13 +22,56 @@ public class App {
     public App(){
         new MainWindow(this);
 
-        Storage storage = new Storage(this);
+        storage = new Storage(this);
 
+        File userDataFile = new File("src/Database-impostor/Users.txt");
+        String usersString = getFileContent(userDataFile);
+
+        String[] usersFromFile = usersString.split("\r\n");
+        users = new Guest[usersFromFile.length];
+
+        for (int i = 0; i < usersFromFile.length; i++) {
+            String userString = usersFromFile[i];
+
+            String[] userData = userString.split(":");
+
+            String name = userData[0];
+            String password = userData[1];
+
+            users[i] = new Guest(name, password);
+        }
     }
+
+    private String getFileContent (File f){
+
+        FileInputStream inputStream;
+
+        try {
+            inputStream = new FileInputStream(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        return sb.toString();
+    }
+
 
     public void subtractMoney(double amount) {
         money -= amount;
     }
+
     public void addMoney(double amount){ money += amount; }
 
     public double getMoney () {
--- src/model/people/Guest.java
+++ src/model/people/Guest.java
@@ -1,5 +1,29 @@
 package model.people;
+
 import model.food.Meal;
 
 public class Guest {
-        }
+
+    private String name;
+    private String password;
+
+    private double budget;
+    private Meal[] lastMeals;
+
+
+    public Guest(String name, String password) {
+        this.name = name;
+        this.password = password;
+        lastMeals = new Meal[0];
+    }
+
+    public void addToLastMeals(Meal newMeal) {
+        Meal[] tmpNewMeals = new Meal[lastMeals.length + 1];
+
+        for (int i = 0; i < lastMeals.length; i++)
+            tmpNewMeals[i] = lastMeals[i];
+
+        tmpNewMeals[lastMeals.length] = newMeal;
+        lastMeals = tmpNewMeals;
+    }
+}
