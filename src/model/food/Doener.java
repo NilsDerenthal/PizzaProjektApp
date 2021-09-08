298cd244a06bb37ad9b0afe4ebb3f86c963a73d0
balzlore <balzlore@kkg-do.eu>
2021-09-08 13:09:23 +0200

    Coded multiple classes in model package

--- src/model/food/Doener.java
+++ src/model/food/Doener.java
@@ -1,4 +1,28 @@
 package model.food;
 
 public class Doener extends Meal {
+
+    private double skewerPercentage = 1;
+    //hardcoded value (25€)
+    private double skewerPrice = 25.00;
+
+    public Doener (App app) {
+        super(app);
+    }
+
+    public void makeDoener(){
+        // hardcoded value (1.5%)
+        if(skewerPercentage > 0.015){
+            skewerPercentage = 1;
+            mainController.subtractMoney(skewerPrice);
+        }
+
+        // hardcoded in value (1.5%)
+        skewerPercentage -= 0.015;
+        //TODO Finish Guest / Order
+        /*
+        return Doener;
+        gotta do stuff in Guest / Order first to be able to do this
+        */
+    }
 }
--- src/model/food/Meal.java
+++ src/model/food/Meal.java
@@ -1,4 +1,11 @@
 package model.food;
 
 public abstract class Meal {
+
+    protected int price;
+    protected App mainController;
+
+    public Meal (App mainController) {
+        this.mainController = mainController;
+    }
 }
