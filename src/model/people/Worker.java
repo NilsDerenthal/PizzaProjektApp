876a38904a9d012511bcb50b8e9a887935250127
fielgeor <fielgeor@kkg-do.eu>
2021-09-14 11:13:12 +0200

    Worker

--- src/model/people/Worker.java
+++ src/model/people/Worker.java
@@ -1,10 +1,27 @@
 package model.people;
 
+
+import control.OrderController;
+import model.Beverage;
+import model.food.Meal;
+
 public class Worker {
 
     //todo
 
-    public Worker(){
+    public Worker(OrderController orderController, Beverage beverage, Meal meal){
 
     }
+    public void prepareFood(){
+
+    }
+    public void getNewOrder(){
+       // return .getMealsInOrder;
+
+    }
+    public void makeNewOrder(){
+
+    }
+
+
 }
