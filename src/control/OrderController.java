3effc5383ad9361895be90cabca8a5aa8fe3c130
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-09-17 15:41:38 +0200

    Order controller documentation

--- src/control/OrderController.java
+++ src/control/OrderController.java
@@ -2,18 +2,40 @@ package control;
 
 public class OrderController {
 
+    // The main controller, passed down
     private final App mainController;
 
+    /**
+     * basic contructor for this Controller
+     * @param mainController the main controller
+     */
     public OrderController(App mainController){
         this.mainController = mainController;
     }
 
+    /**
+     * Adds the specified food to the current order.
+     * @param orderType One of Pizza, Doener, Beverage and Salad
+     * @param type the specified type of Pizza/Beverage. Can be null for salad and Doener
+     * @param isFav inidicates wheter this should be added to favourites or normal order
+     */
     public void addFoodToOrder (String orderType, String type, boolean isFav){ mainController.getWorker().addFoodToOrder(orderType,type,isFav); }
 
+    /**
+     * Orders the favourite meal
+     */
     public void orderFavMeal(){ mainController.getWorker().orderFavMeal(); }
 
+    /**
+     * Adds the current order to the last order list.
+     */
     public void addToLastOrders(){ mainController.getWorker().addToLastOrders(); }
 
+    /**
+     * Gets the possible types of the specified order.
+     * @param meal the meal for which the orders are to be listed. One of Pizza, Doener, Salad and Beverage
+     * @return the available options
+     */
     public String[] getOrderTypes(String meal) {
         return switch (meal) {
             case "Pizza" -> new String[]{
