2844afa78f1df2ba902499719f3dff3005c873f1
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-09-18 10:56:04 +0200

    documentation

--- src/view/LoginPanel.java
+++ src/view/LoginPanel.java
@@ -7,12 +7,19 @@ import javax.swing.*;
 
 public class LoginPanel extends ViewPanel{
 
-    private JTextField usernameTextField;
     private JPanel mainPanel;
+
+    private JTextField usernameTextField;
     private JPasswordField passwordField;
+
     private JButton backButton;
     private JButton logInButton;
 
+    /**
+     * Panel for viewing the log-in
+     * @param viewController the viewController Object passed as a reference
+     * @param signUpPanel if this panel is a sign up panel or a log-in panel. This decides button labeling and actions.
+     */
     public LoginPanel (ViewController viewController, boolean signUpPanel) {
         super(viewController);
 
@@ -31,12 +38,10 @@ public class LoginPanel extends ViewPanel{
 
             passwordField.setText(null);
 
-            if (response == null) {
+            if (response == null)
                 usernameTextField.setText(null);
-            } else {
+            else
                 JOptionPane.showMessageDialog(null, response);
-            }
-
         });
 
         backButton.addActionListener(e -> viewController.setPanel("startPanel"));
@@ -44,6 +49,7 @@ public class LoginPanel extends ViewPanel{
         passwordField.addActionListener(e -> logInButton.doClick());
     }
 
+    @Override
     public JPanel getMainPanel() {
         return mainPanel;
     }
