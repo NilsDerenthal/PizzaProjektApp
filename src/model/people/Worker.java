7e597cb3b760f8c3049729135eef15bf1fb5e6f2
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-09-18 11:01:19 +0200

    documentation

--- src/model/people/Worker.java
+++ src/model/people/Worker.java
@@ -10,16 +10,35 @@ public class Worker {
 
     private final App mainController;
 
+    /**
+     * Creates a worker
+     * @param mainController the mainController reference
+     */
     public Worker(App mainController){
         this.mainController = mainController;
     }
 
+    /**
+     * Makes the food that is the current order of the guest.
+     * @param guest the guest who's food is to be made
+     */
     public void prepareFood(Guest guest){
         guest.getOrder(guest.getOrder(false).length - 1,false).makeIt();
     }
 
+    /**
+     * Adds a beverage to the current user's order.
+     * @param type the type of the order
+     * @param isFav if this is to be added to the favourite order
+     */
     public void addBeverageToOrder(String type,boolean isFav){ mainController.getCurrentUser().addBeverageToOrder(new Beverage(mainController, type),isFav);}
 
+    /**
+     * Adds a
+     * @param orderType the type of order to be added
+     * @param type the type of the order to be added
+     * @param isFav if this is to be added to favourites
+     */
     public void addFoodToOrder (String orderType, String type, boolean isFav){
         Guest guest = mainController.getCurrentUser();
 
@@ -28,14 +47,23 @@ public class Worker {
             case "Doener" -> new Doener(mainController);
             case "Salad" -> new Salad(mainController);
             case "Beverage" -> new Beverage(mainController, type);
-            default -> throw new IllegalStateException("Unexpected type: " + orderType);
-        },isFav);
 
-        if(!isFav) prepareFood(guest);
+            default -> throw new IllegalStateException("Unexpected type: " + orderType);
+
+        } , isFav);
+
+        if(!isFav)
+            prepareFood(guest);
     }
 
+    /**
+     * Adds to the last orders
+     */
     public void addToLastOrders(){ mainController.getCurrentUser().addToLastOrders(); }
 
+    /**
+     * Orders the favourite meal of the current user
+     */
     public void orderFavMeal(){ mainController.getCurrentUser().orderFavMeal(); }
 
 
